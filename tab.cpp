c821f1e80c4d76b232ca98777c31996c9a79dedb
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-12-11 09:18:22 -0500

    Create tab.cpp

---
+++ tab.cpp
@@ -0,0 +1,18 @@
+
+//define these outside of the function you have the tabs under
+//определите их вне функции, в которой у вас есть вкладки
+int currentTab = 0;
+int numTabs = 3;
+const char* TabList[] = { "Aim", "Visual", "Misc" };
+
+//main tab system
+//система основных вкладок
+float TBS = ImGui::GetContentRegionAvail().x;
+for (int i = 0; i < numTabs; i++)
+{
+	if (ImGui::Button(TabList[i], ImVec2(TBS / numTabs - 5.f, 30)))
+		currentTab = i;
+
+	if (i < 2)
+		ImGui::SameLine();
+}
