4670dca13fe0bf3cfdb67d74b669478ddb6e896d
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-12-14 22:14:24 -0500

    Update tab.cpp

--- tab.cpp
+++ tab.cpp
@@ -1,24 +1,34 @@
-
-//define these outside of the function you have the tabs under
-//определите их вне функции, в которой у вас есть вкладки
+// define these outside of the function you have the tabs under
+// определите их вне функции, под которой у вас есть вкладки
 int currentTab = 0;
-int numTabs = 3;
 const char* TabList[] = { "Aim", "Visual", "Misc" };
+int numTabs = sizeof(TabList) / sizeof(TabList[0]);
 
-//main tab system
-//система основных вкладок
+// main tab system
+// система основных вкладок
 float TBS = ImGui::GetContentRegionAvail().x;
 for (int i = 0; i < numTabs; i++)
 {
-	if (ImGui::Button(TabList[i], ImVec2(TBS / numTabs - 5.f, 30)))
-		currentTab = i;
+    if (ImGui::Button(TabList[i], ImVec2(TBS / numTabs - 5.f, 30)))
+        currentTab = i;
 
-	if (i < 2)
-		ImGui::SameLine();
+    if (i < 2)
+        ImGui::SameLine();
 }
 
-// how to use 
+// how to use
 // как использовать
-if (currentTab == 0) { }
-else if (currentTab == 1) { }
-else if (currentTab == 2) { }
+switch (currentTab)
+{
+case 0:
+    ImGui::Text("Aim");
+    break;
+
+case 1:
+    ImGui::Text("Visual");
+    break;
+
+case 2:
+    ImGui::Text("Misc");
+    break;
+}
